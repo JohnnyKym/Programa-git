9cf294bc064c3212ca18e652732979a9964d7fdf
JohnnyKym <santiagonu2001@hotmail.com>
2024-10-22 19:02:06 -0500

    primera version

---
+++ Evidencia/script.js
@@ -0,0 +1,23 @@
+document.getElementById('colorButton').addEventListener('click', function() {
+    
+  
+    document.body.style.backgroundColor = getRandomColor();
+    });
+    
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        
+        f
+    for (let i = 0; i < 6; i++) {
+            color += letters[
+            color += l
+    
+            co
+    Math.floor(Math.random() * 16)];
+        }
+        
+        }
+      
+    return color;
+    }
